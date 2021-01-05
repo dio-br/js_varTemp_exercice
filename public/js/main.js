34013c45be726ba135f193409a190ed5df2e7441
Mohammed <el-med7@outlook.com>
2021-01-05 11:40:36 +0100

    exo temp

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+// let a = 1; //-> 2
+// let b = 2; //-> 3
+// let c = 3; //-> 1
+
+// let temp;
+// temp = a
+// a = b
+// b = c 
+// c = temp
+
+// console.log(a, b, c);
+
+    //   EXO1  //
+
+// let a = 1; // -> 4
+// let b = 2; // -> 3
+// let c = 3; // -> 2
+// let d = 4; // -> 1
+
+// let temp1
+// temp1 = a
+// temp2 = b
+// a = d
+// b = c
+// c = temp2
+// d = temp1
+
+// console.log(a, b, c, d);
+
+    //   EXO2   //
+
+// let a = 1; // -> 2
+// let b = 2; // -> 3
+// let c = 3; // -> 4
+// let d = 4; // -> 1
+
+// let temp1 
+// temp1 = a
+// a = b
+// b = c
+// c = d
+// d = temp1
+
+// console.log(a, b, c, d);
+
+    // EXO3  //
+
+// let a = 4; 
+// let b = 3;
+// let c = 2; 
+// let d = 1; 
+
+// let tab = [a, b, c, d]
+// console.log(tab.reverse())
+
+//     EXO4     //
+
+let a = 1 // -> 4
+let b = 2 // -> 5
+let c = 3 // -> 1 a
+let d = 4 // -> 2 b
+let e = 5 // -> 6
+let f = 6 // -> 3
+
+let temp1;
+let temp2;
+let temp3;
+temp1 = a
+temp2 = b
+temp3 = c
+a = d
+b = e
+c = temp1
+d = temp2
+e = f
+f = temp3
+
+console.log(a, b, c, d, e, f);
+
+
+
